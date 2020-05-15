bf74f537138c99baa2abbb1739d9d4df79db2f68
ogtay <ogtayhuseynov0@gmail.com>
2020-05-15 23:17:38 +0400

    layaout creation

---
+++ components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="flex flex-col h-screen min-h-screen">
+  <div class="flex items-center justify-between px-4 bg-gray-900 text-gray-200 h-12">
+      <div>
+      </div>
+      <div>
+      </div>
+  </div>
+  <div class="flex  flex-1 overflow-y-hidden">
+    <div class="flex w-64  overflow-y-auto border-r-2" :class="sidenav ? '': 'hidden'">
+      
+    </div>
+    <div class=" flex-1 overflow-y-auto">
+asd
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+data () {
+  return {
+    sidenav: true
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NavBar/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar"
+export default {
+  components: {NavBar}
+}
+</script>
